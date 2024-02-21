65103b5eeca1e01deb086a035acc8626a9cdae4c
Tran Thuy <tranthithuyr789@gmail.com>
2024-02-21 16:48:52 +0700

    Data Driven Testing

--- testdata/com/nopcommerce/data/UserData.java
+++ testdata/com/nopcommerce/data/UserData.java
@@ -1,16 +1,47 @@
 package com.nopcommerce.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import commons.GlobalConstants;
+import org.eclipse.core.internal.utils.ObjectMap;
+
+import java.io.File;
+
 public class UserData {
-    public class Register{
+    @JsonProperty("firstName")
+    private String firstName;
+    @JsonProperty("lastName")
+    private String lastName;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("password")
+    private String password;
 
+    public static UserData getUserData() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+            return mapper.readValue(new File(GlobalConstants.PROJECT_PATH+"/resources/userData.json"), UserData.class);
+        }catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
-    public class Login{
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
     }
-    public class Address{
 
+    public String getEmail() {
+        return email;
     }
-    public class MyAccount{
 
+    public String getPassword() {
+        return password;
     }
+
 }
